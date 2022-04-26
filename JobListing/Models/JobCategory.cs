b3cbc748d54707e308f8200a1f376e55f5bf5092
Anik Biswas <anik.biswas@eps.com.bd>
2022-04-26 23:53:39 +0600

    init commit

---
+++ JobListing/Models/JobCategory.cs
@@ -0,0 +1,11 @@
+﻿namespace JobListing.Models
+{
+    public class JobCategory
+    {
+        public int Id { get; set; }
+        public string CategoryName { get; set; }
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+        public DateTime? DateUpdated { get; set; }
+
+    }
+}
---
+++ JobListing/Models/JobTag.cs
@@ -0,0 +1,11 @@
+﻿namespace JobListing.Models
+{
+    public class JobTag
+    {
+        public int Id { get; set; }
+        public string TagName { get; set; }
+        public DateTime DateAdded { get; set; } = DateTime.Now;
+        public DateTime? DateUpdated { get; set; }
+
+    }
+}
